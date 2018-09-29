7e4352e2932b3299b0f72e9eca01f9ef0a097864
br-monteiro <edson.monteiro@chaordicsystems.com>
2018-09-29 11:46:42 -0300

    Adding Paginator Helper

---
+++ App/Helpers/PaginatorHelper.php
@@ -0,0 +1,42 @@
+<?php
+namespace App\Helpers;
+
+use HTR\Database\EntityAbstract as db;
+use Slim\Http\Request;
+use Slim\Http\Response;
+use App\System\Configuration as cfg;
+use Doctrine\DBAL\DBALException;
+
+class PaginatorHelper
+{
+
+    /**
+     * Build the attributes necessary to paginate the query
+     * @author Edson B S Monteiro <bruno.monteirodg@gmail.com>
+     * @param Request $request
+     * @param string $entityName
+     * @param string $field
+     * @return \stdClass
+     */
+    public static function buildAttributes(Request $request, string $entityName, string $field = 'id'): \stdClass
+    {
+        $stdResult = new \stdClass();
+        $limit = $request->getParam('limit');
+        $offset = $request->getParam('offset');
+        $stdResult->limit = $limit ? intval($limit) : null;
+        $stdResult->offset = $offset ? intval($offset) : null;
+
+        try {
+            $allResults = db::em()->getConnection()->query("SELECT {$field} FROM {$entityName}")->rowCount();
+        } catch (DBALException $ex) {
+            $error = $ex->getMessage();
+            $allResults = 0;
+        }
+
+        $stdResult->hasError = isset($error);
+        $stdResult->error = $error ?? '';
+        $stdResult->allResults = $allResults;
+
+        return $stdResult;
+    }
+}
